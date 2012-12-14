3ec18a51bdabe8b654f3985043e6ebebc7c50271
Edoardo Bezzeccheri <coolman3@gmail.com>
2012-12-14 11:57:49 +0100

    Aggiunta base libreria ed eliminati file non necessari

---
+++ libohiboard/include/libohiboard.h
@@ -0,0 +1,13 @@
+/*
+ * libohiboard.h
+ *
+ *  Created on: 14/dic/2012
+ *      
+ */
+
+#ifndef LIBOHIBOARD_H_
+#define LIBOHIBOARD_H_
+
+void testLibrary();
+
+#endif /* LIBOHIBOARD_H_ */
---
+++ libohiboard/libohiboard.c
@@ -0,0 +1,14 @@
+/*
+ * libohiboard.c
+ *
+ *  Created on: 14/dic/2012
+ *      
+ */
+
+#include "libohiboard.h"
+
+void testLibrary()
+{
+	int i = 0;
+	i++;
+}
