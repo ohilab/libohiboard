3a8ec37ecc0ff0be3bbf11e0dedaf0027b6dce4a
Marco Giammarini <m.giammarini@warcomeb.it>
2013-07-11 16:37:03 +0200

    Added utility.h file for useful functions and definitions.

---
+++ include/utility.h
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * Copyright (C) 2012-2013 A. C. Open Hardware Ideas Lab
+ * 
+ * Author(s):
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ *  
+ * Project: libohiboard
+ * Package: -
+ * Version: 0.0
+ * 
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************/
+
+/**
+ * @file libohiboard/include/utility.h
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ * @brief SPI definitions and prototypes
+ */
+
+#ifndef __UTILITY_H
+#define __UTILITY_H
+
+/* Useful define */
+#define GPIO_PIN_MASK            0x1Fu
+#define GPIO_PIN(x)              (((1)<<(x & GPIO_PIN_MASK)))
+
+#endif /* __UTILITY_H */
