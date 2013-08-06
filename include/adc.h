087b6d967c2af4e392c570222ab9dc8b50e735f7
Marco Giammarini <m.giammarini@warcomeb.it>
2013-08-06 17:07:35 +0200

    First implementation of ADC module.

--- include/adc.h
+++ include/adc.h
@@ -35,12 +35,61 @@
 #include "errors.h"
 #include "types.h"
 
+typedef enum {
+	ADC_RESOLUTION_16BIT,
+	ADC_RESOLUTION_12BIT,
+	ADC_RESOLUTION_10BIT,
+	ADC_RESOLUTION_8BIT,	
+} Adc_Resolution;
+
+typedef enum {
+    ADC_AVARAGE_1_SAMPLES,
+    ADC_AVARAGE_4_SAMPLES,
+    ADC_AVARAGE_8_SAMPLES,
+    ADC_AVARAGE_16_SAMPLES,
+    ADC_AVARAGE_32_SAMPLES,    
+} Adc_Avarage;
+
+typedef enum {
+#if defined (FRDMKL25Z)
+	ADC_CH_SE0     = 0x00,
+	ADC_CH_SE3     = 0x03,
+	ADC_CH_SE4b    = 0x04,
+	ADC_CH_SE5b    = 0x05,
+	ADC_CH_SE6b    = 0x06,
+	ADC_CH_SE7b    = 0x07,
+	ADC_CH_SE8     = 0x08,
+	ADC_CH_SE9     = 0x09,
+	ADC_CH_SE11    = 0x0B,
+	ADC_CH_SE12    = 0x0C,
+	ADC_CH_SE13    = 0x0D,
+	ADC_CH_SE14    = 0x0E,
+	ADC_CH_SE15    = 0x0F,
+	ADC_CH_SE23    = 0x17,
+	ADC_CH_TEMP    = 0x1A,
+	ADC_CH_BANDGAP = 0x1B,
+	ADC_CH_VREFH   = 0x1D,
+	ADC_CH_VREFL   = 0x1E,
+	ADC_CH_DISABLE = 0x1F
+#elif defined(MKL15Z4)
+#elif defined(MK60DZ10)
+#elif defined(FRDMKL05Z)
+#elif defined(FRDMK20D50M)
+#endif
+} Adc_ChannelNumber;
+
 typedef struct Adc_Device* Adc_DeviceHandle;
+
+System_Errors Adc_init (Adc_DeviceHandle dev);
+
+void Adc_setResolution (Adc_DeviceHandle dev, Adc_Resolution resolution);
+
 
 #if defined(MKL15Z4) || defined(FRDMKL25Z)
 extern Adc_DeviceHandle ADC0;
 #elif defined(MK60DZ10)
 #elif defined(FRDMKL05Z)
+#elif defined(FRDMK20D50M)
 #endif
 
 #endif /* __ADC_H */
--- source/adc.c
+++ source/adc.c
@@ -40,14 +40,137 @@
 typedef struct Adc_Device {
     ADC_MemMapPtr 		  regMap;
     
-    uint8_t               pinEnabled;
+    Adc_Resolution        resolution;
+    Adc_Avarage           avarage;
+    
+//    uint8_t               pinEnabled;
 } Adc_Device;
 
 #if defined(MKL15Z4) || defined(FRDMKL25Z)
 static Adc_Device adc0 = {
         .regMap           = ADC0_BASE_PTR,
+        .resolution       = ADC_RESOLUTION_8BIT,
+        .avarage          = ADC_AVARAGE_1_SAMPLES
 };
 Adc_DeviceHandle ADC0 = &adc0; 
 #elif defined(MK60DZ10)
 #elif defined(FRDMKL05Z)
+#elif defined(FRDMK20D50M)
 #endif
+
+/**
+ * @brief
+ * 
+ * Fixed conditions:
+ *   - Long sample enabled.
+ *   - High speed disabled.
+ *   - Bus clock selected and divided-by-2.
+ *   - Single conversion.
+ * 
+ * TODO: Pin manage!  
+ * 
+ * @param dev Adc device handle to be synchronize.
+ * @return A System_Errors elements that indicate the status of initialization.
+ */
+System_Errors Adc_init (Adc_DeviceHandle dev)
+{
+    ADC_MemMapPtr regmap = dev->regMap;
+
+//    if (dev->pinEnabled == IIC_PIN_DISABLED)
+//    	return ERRORS_HW_NOT_ENABLED;
+    
+    /* Turn on clock */
+#if defined(MKL15Z4) || defined(FRDMKL25Z)
+    if (regmap == ADC0_BASE_PTR)
+        SIM_SCGC6 |= SIM_SCGC6_ADC0_MASK;
+    else
+        return ERRORS_PARAM_VALUE;
+#elif defined(MK60DZ10)
+#elif defined(FRDMKL05Z)
+#elif defined(FRDMK20D50M)
+#endif
+
+    /* Long sample time, busclock/2 and divide by-1*/
+    ADC_CFG1_REG(regmap) = ADC_CFG1_ADIV(0) | 
+            ADC_CFG1_ADLSMP_MASK | ADC_CFG1_ADICLK(1);
+    
+    switch (dev->resolution)
+    {
+    case ADC_RESOLUTION_8BIT:
+        ADC_CFG1_REG(regmap) |= ADC_CFG1_MODE(0);
+        break;
+    case ADC_RESOLUTION_10BIT:
+        ADC_CFG1_REG(regmap) |= ADC_CFG1_MODE(1);
+        break;
+    case ADC_RESOLUTION_12BIT:
+        ADC_CFG1_REG(regmap) |= ADC_CFG1_MODE(2);
+        break;
+    case ADC_RESOLUTION_16BIT:
+        ADC_CFG1_REG(regmap) |= ADC_CFG1_MODE(3);
+        break;
+	}
+    
+    /* 24 ADCK cycles total: Long sample time */
+    ADC_CFG2_REG(regmap) = ADC_CFG2_MUXSEL_MASK | ADC_CFG2_ADLSTS(0);
+    
+    /* Select voltage reference: default */
+    ADC_SC2_REG(regmap) = ADC_SC2_REFSEL(0);
+    
+    /* Select the avarage */
+    switch (dev->avarage)
+    {
+    case ADC_AVARAGE_1_SAMPLES:
+        /* Nothing to do! */
+        break;
+    case ADC_AVARAGE_4_SAMPLES:
+        ADC_SC3_REG(regmap) = ADC_SC3_AVGE_MASK | ADC_SC3_AVGS(0);
+        break;
+    case ADC_AVARAGE_8_SAMPLES:
+        ADC_SC3_REG(regmap) = ADC_SC3_AVGE_MASK | ADC_SC3_AVGS(1);
+        break;
+    case ADC_AVARAGE_16_SAMPLES:
+        ADC_SC3_REG(regmap) = ADC_SC3_AVGE_MASK | ADC_SC3_AVGS(2);
+        break;
+    case ADC_AVARAGE_32_SAMPLES:
+        ADC_SC3_REG(regmap) = ADC_SC3_AVGE_MASK | ADC_SC3_AVGS(3);
+        break;
+    }
+
+	return ERRORS_NO_ERROR;
+}
+
+System_Errors Adc_readValue (Adc_DeviceHandle dev, Adc_ChannelNumber channel, uint16_t *value)
+{
+    ADC_MemMapPtr regmap = dev->regMap;
+
+    if (channel != ADC_CH_DISABLE)
+    {
+        /* Start conversion */
+        ADC_SC1_REG(regmap,0) = ADC_SC1_ADCH(channel);
+    
+        /* wait until conversion ended */
+        while ((ADC_SC1_REG(regmap,0) & ADC_SC1_COCO_MASK) != ADC_SC1_COCO_MASK);
+    
+        *value = (uint16_t) ADC_R_REG(regmap,0);
+    
+        /* Disable conversion */
+        ADC_SC1_REG(regmap,0) = ADC_SC1_ADCH(ADC_CH_DISABLE);
+
+        return ERRORS_NO_ERROR;
+    }
+    else
+    {
+        *value = 0;
+        return ERRORS_ADC_CHANNEL_WRONG;
+    }
+}
+
+void Adc_setResolution (Adc_DeviceHandle dev, Adc_Resolution resolution)
+{
+	dev->resolution = resolution;
+}
+
+void Adc_setAvarage (Adc_DeviceHandle dev, Adc_Avarage avarage)
+{
+    dev->avarage = avarage;
+}
