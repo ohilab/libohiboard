376abf9661c2d5abe8766e98823a41d58a8b3877
Marco Giammarini <m.giammarini@warcomeb.it>
2013-08-06 07:36:49 +0200

    Added file for RTC module.

---
+++ include/rtc.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Copyright (C) 2012-2013 A. C. Open Hardware Ideas Lab
+ * 
+ * Author(s):
+ *	Marco Giammarini <m.giammarini@warcomeb.it>
+ *	
+ * Project: libohiboard
+ * Package: RTC
+ * Version: 0.0
+ * 
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************/
+
+/**
+ * @file libohiboard/include/rtc.h
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ * @brief RTC definitions and prototypes.
+ */
+
+#ifndef __RTC_H
+#define __RTC_H
+
+#include "platforms.h"
+#include "errors.h"
+#include "types.h"
+
+
+
+
+#endif /* __RTC_H */
---
+++ source/rtc.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+ * Copyright (C) 2012-2013 A. C. Open Hardware Ideas Lab
+ * 
+ * Author(s):
+ *	Marco Giammarini <m.giammarini@warcomeb.it>
+ *	
+ * Project: libohiboard
+ * Package: RTC
+ * Version: 0.0
+ * 
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************/
+
+/**
+ * @file libohiboard/source/rtc.h
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ * @brief RTC functions implementation.
+ */
+
+#include "rtc.h"
+
+typedef struct Rtc_Device {
+    RTC_MemMapPtr 		  regMap;
+} Rtc_Device;
