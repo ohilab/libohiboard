5b0f8d2443dc407600c46918646fa2542ef58a93
Marco Giammarini <m.giammarini@warcomeb.it>
2014-01-09 14:12:18 +0100

    Added assert function for errors control.

---
+++ source/errors.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * Copyright (C) 2012-2013 A. C. Open Hardware Ideas Lab
+ * 
+ * Author(s):
+ *  Edoardo Bezzeccheri <coolman3@gmail.com>
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ *  
+ * Project: libohiboard
+ * Package: -
+ * Version: 0.0
+ * 
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************/
+
+/**
+ * @file libohiboard/include/errors.h
+ * @author Edoardo Bezzeccheri <coolman3@gmail.com>
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ * @brief Errors definition
+ */
+
+#include "errors.h"
+
+void Errors_assert (const char* file, const int line)
+{
+    /* Set breakpoint to control the execution! */
+}
