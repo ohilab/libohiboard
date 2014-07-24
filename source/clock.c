a88640f66345a62a841a28d025db9ee19385004e
Marco Giammarini <m.giammarini@warcomeb.it>
2014-07-24 16:45:39 +0200

    Added clock generation at 100MHz for MK10D10 Fix it with customizable solutions

--- source/clock.c
+++ source/clock.c
@@ -37,7 +37,7 @@ uint32_t Clock_getFrequency (Clock_Source source)
     return 50000000; /* TODO: FIXME: */
 #elif defined (FRDMKL25Z)
     return 40000000; /* TODO: FIXME: */
-#elif defined (MK10DZ10)
+#elif defined (MK10DZ10) || defined (MK10D10) 
     return 50000000; /* TODO: FIXME: */
 #endif
 }
