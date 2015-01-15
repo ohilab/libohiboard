3cda55a8e6dc131f905126827d62e5f6acaf1f7a
Alessio Paolucci <a.paolucci89@gmail.com>
2015-01-15 18:07:28 +0100

    Modified clock.c for compatibility with clock_kl25.c #15

--- source/clock.c
+++ source/clock.c
@@ -29,7 +29,16 @@
  * @brief Clock implementations.
  */
 
+#include "platforms.h"
 #include "clock.h"
+
+#if defined(MKL15Z4) || defined(MK60DZ10) ||  \
+	defined(FRDMKL05Z) || defined(MK60F15) || \
+	defined(FRDMKL02Z) || defined(MKL02Z4) || \
+	defined(MK10DZ10) || defined(MK10D10) ||  \
+	defined(MKL03Z4) || defined(FRDMKL03Z) || \
+	defined(OHIBOARD_R1) || defined(FRDMK20D50M) || \
+	defined(MK64F12)
 
 uint32_t Clock_getFrequency (Clock_Source source)
 {
@@ -64,5 +73,8 @@ uint32_t Clock_getFrequency (Clock_Source source)
 #elif defined (FRDMKL03Z)
         return 47972352; /* TODO: FIXME: */
 #endif    
+        break;
     }
 }
+
+#endif
