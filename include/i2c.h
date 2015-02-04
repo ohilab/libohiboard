89f264495945222554842eb4a5e7eb8bc952e085
Marco Giammarini <m.giammarini@warcomeb.it>
2015-02-04 10:36:26 +0100

    Splitted I2C code into one file for each microcontrollers for KL15 and KL25 #8.

--- include/i2c.h
+++ include/i2c.h
@@ -1,25 +1,25 @@
-/******************************************************************************
- * Copyright (C) 2012-2013 A. C. Open Hardware Ideas Lab
- * 
- * Author(s):
- *	Marco Giammarini <m.giammarini@warcomeb.it>
- *	
- * Project: libohiboard
- * Package: I2C
- * Version: 0.0
- * 
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * any later version.
+/* Copyright (C) 2012-2015 A. C. Open Hardware Ideas Lab
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>
+ * Authors:
+ *   Marco Giammarini <m.giammarini@warcomeb.it>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  ******************************************************************************/
 
 /**
@@ -55,13 +55,44 @@ typedef enum {
     IIC_NO_LAST_BYTE
 } Iic_LastByteMode;
 
-typedef struct Iic_Device* Iic_DeviceHandle;
-
-#if defined(MK64F12) || defined(FRDMK64F)
-
 typedef enum
 {
-#if defined (MK64F12) || defined (FRDMK64F)
+#if defined (LIBOHIBOARD_KL03Z4)     || \
+    defined (LIBOHIBOARD_FRDMKL03Z)
+
+#elif defined (LIBOHIBOARD_KL15Z4)
+
+    IIC_PINS_PTA3,
+
+    IIC_PINS_PTB0,
+    IIC_PINS_PTB2,
+
+    IIC_PINS_PTC1,
+    IIC_PINS_PTC8,
+    IIC_PINS_PTC10,
+
+    IIC_PINS_PTE1,
+    IIC_PINS_PTE19,
+    IIC_PINS_PTE24,
+
+#elif defined (LIBOHIBOARD_KL25Z4) || \
+      defined (LIBOHIBOARD_FRDMKL25Z)
+
+    IIC_PINS_PTA3,
+
+    IIC_PINS_PTB0,
+    IIC_PINS_PTB2,
+
+    IIC_PINS_PTC1,
+    IIC_PINS_PTC8,
+    IIC_PINS_PTC10,
+
+    IIC_PINS_PTE1,
+    IIC_PINS_PTE24,
+
+#elif defined (LIBOHIBOARD_K64F12)     || \
+      defined (LIBOHIBOARD_FRDMK64F)
+
 	IIC_PINS_PTA12,
 	IIC_PINS_PTA14,
 
@@ -79,11 +110,48 @@ typedef enum
 #endif
 
 	IIC_PINS_SCLNONE,
+
 } Iic_SclPins;
 
 typedef enum
 {
-#if defined (MK64F12) || defined (FRDMK64F)
+
+#if defined (LIBOHIBOARD_KL03Z4)     || \
+    defined (LIBOHIBOARD_FRDMKL03Z)
+
+#elif defined (LIBOHIBOARD_KL15Z4)
+
+    IIC_PINS_PTA4,
+
+    IIC_PINS_PTB1,
+    IIC_PINS_PTB3,
+
+    IIC_PINS_PTC2,
+    IIC_PINS_PTC9,
+    IIC_PINS_PTC11,
+
+    IIC_PINS_PTE0,
+    IIC_PINS_PTE18,
+    IIC_PINS_PTE25,
+
+#elif defined (LIBOHIBOARD_KL25Z4) || \
+      defined (LIBOHIBOARD_FRDMKL25Z)
+
+    IIC_PINS_PTA4,
+
+    IIC_PINS_PTB1,
+    IIC_PINS_PTB3,
+
+    IIC_PINS_PTC2,
+    IIC_PINS_PTC9,
+    IIC_PINS_PTC11,
+
+    IIC_PINS_PTE0,
+    IIC_PINS_PTE25,
+
+#elif defined (LIBOHIBOARD_K64F12)     || \
+      defined (LIBOHIBOARD_FRDMK64F)
+
 	IIC_PINS_PTA11,
 	IIC_PINS_PTA13,
 
@@ -97,9 +165,11 @@ typedef enum
 
 	IIC_PINS_PTE0,
 	IIC_PINS_PTE25,
+
 #endif
 
 	IIC_PINS_SDANONE,
+
 } Iic_SdaPins;
 
 typedef struct _Iic_Config
@@ -113,23 +183,11 @@ typedef struct _Iic_Config
 
     uint16_t              sclTimeout;
 
-//    uint8_t               pinEnabled;
 } Iic_Config;
 
-#endif
+typedef struct Iic_Device* Iic_DeviceHandle;
 
-#if defined (MK64F12) || defined (FRDMK64F)
 System_Errors Iic_init (Iic_DeviceHandle dev, Iic_Config *config);
-//#elif defined(MKL15Z4) || defined(FRDMK20D50M) || defined(FRDMKL05Z) || defined(MK60F15) || \
-//	  defined(FRDMKL02Z) || defined(MKL02Z4) || defined(MK10DZ10) || defined(MK10D10) || \
-//	  defined(MK60DZ10) || defined(MK60F15) || defined(MKL03Z4) || defined(FRDMKL03Z) || \
-//	  defined(OHIBOARD_R1)
-#elif !defined (MK64F12) && !defined (FRDMK64F)
-System_Errors Iic_init (Iic_DeviceHandle dev);
-
-System_Errors Iic_setBaudRate (Iic_DeviceHandle dev, uint32_t br);
-System_Errors Iic_setDeviceType (Iic_DeviceHandle dev, Iic_DeviceType devType);
-#endif
 
 void Iic_pinEnabled (Iic_DeviceHandle dev);
 
@@ -144,24 +202,33 @@ System_Errors Iic_readByte (Iic_DeviceHandle dev, uint8_t *data,
 System_Errors Iic_readBytes (Iic_DeviceHandle dev, uint8_t address, 
         uint8_t *data, uint8_t length, Iic_StopMode stopRequest);
 
-#if !defined(FRDMKL02Z) && !defined(MKL02Z4) && \
-	!defined(FRDMKL03Z) && !defined(MKL03Z4)
+#if 0
 System_Errors Iic_setSclTimeout (Iic_DeviceHandle dev, uint32_t usDelay);
 void Iic_resetSclTimeout (Iic_DeviceHandle dev);
 System_Errors Iic_isToggleSclTimeout (Iic_DeviceHandle dev);
 #endif
 
-#if defined(LIBOHIBOARD_KL15Z4) || defined(FRDMKL25Z) || defined(MK10D10)
+#if defined (LIBOHIBOARD_KL03Z4)     || \
+    defined (LIBOHIBOARD_FRDMKL03Z)
+
+#elif defined (LIBOHIBOARD_KL15Z4)
+
 extern Iic_DeviceHandle IIC0;
 extern Iic_DeviceHandle IIC1;
-#elif defined(MK60DZ10)
+
+#elif defined (LIBOHIBOARD_KL25Z4) || \
+	  defined (LIBOHIBOARD_FRDMKL25Z)
+
 extern Iic_DeviceHandle IIC0;
-#elif defined(FRDMKL05Z)
-extern Iic_DeviceHandle IIC0;
-#elif defined(MK64F12) || defined(FRDMK64F)
+extern Iic_DeviceHandle IIC1;
+
+#elif defined (LIBOHIBOARD_K64F12)     || \
+      defined (LIBOHIBOARD_FRDMK64F)
+
 extern Iic_DeviceHandle IIC0;
 extern Iic_DeviceHandle IIC1;
 extern Iic_DeviceHandle IIC2;
+
 #endif
 
 #endif /* __I2C_H */
