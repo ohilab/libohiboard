2210a91708c8f45f18b75890be249fa07db7912b
matteo.civale <matteo.civale@gmailo.com>
2016-09-01 09:16:05 +0200

    Added ethernet utility for network configuration. note: in "utylity.h" file, SET_BIT(n) macro was commented because may be a  conflict with the same macro defined in "modbus_tcp.h" file

---
+++ source/ethernet-utility.c
@@ -0,0 +1,19 @@
+#ifdef LIBOHIBOARD_ETHERNET
+
+#include "ethernet-utility.h"
+
+void Ethernet_networkConfig(struct netif *netif, Network_Config *config)
+{
+
+    lwip_init();
+    lwipTimerInit(config->pit);
+
+    LWIP_K64F12_setMacAddress(config->macAdd);
+
+    // Initialize network interface
+    netif_add(netif, &config->ip, &config->netMask, &config->gw, 0, LWIP_K64F12_init, ethernet_input);
+    netif_set_default(netif);
+    netif_set_up(netif);
+}
+
+#endif /* LIBOHIBOARD_ETHERNET */
