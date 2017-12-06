d67b21b32e96eaed2e6c5f28e25ee6e7753ebe47
matteo civale <matteo.civale@gmail.com>
2017-12-06 18:27:33 +0100

    Added define for porting of libohiboard in mcuxpresso ide

--- include/types.h
+++ include/types.h
@@ -34,12 +34,16 @@
 
 typedef unsigned char      uint8_t;
 typedef unsigned short     uint16_t;
+#ifndef __MCUXPRESSO
 typedef unsigned long      uint32_t;
+#endif
 typedef unsigned long long uint64_t;
 
 typedef signed char        int8_t;
 typedef short              int16_t;
+#ifndef __MCUXPRESSO
 typedef long               int32_t;
+#endif
 typedef long long          int64_t;
 
 typedef uint8_t            bool;
