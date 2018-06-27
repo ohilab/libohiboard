1927523d90b27f4b5690a302b1b4549c58a02b59
Marco Giammarini <m.giammarini@warcomeb.it>
2018-06-27 18:17:10 +0200

    Small fix on function pointer check to avoid default handler

--- source/ethernet-utility.c
+++ source/ethernet-utility.c
@@ -54,11 +54,13 @@ void Ethernet_networkConfig (struct netif *netif, Ethernet_NetworkConfig *config
 
     // Initialize network interface
     netif_add(netif, &config->ip, &config->mask, &config->gateway, 0, LWIPPorting_init, ethernet_input);
-#if LWIP_NETIF_STATUS_CALLBACK==1
-    netif_set_status_callback(netif,config->netif_status_callback);
+#if LWIP_NETIF_STATUS_CALLBACK == 1
+    if (config->netif_status_callback != 0)
+    	netif_set_status_callback(netif,config->netif_status_callback);
 #endif
-#if LWIP_NETIF_LINK_CALLBACK==1
-    netif_set_link_callback(netif,config->netif_link_callback);
+#if LWIP_NETIF_LINK_CALLBACK == 1
+    if (config->netif_link_callback != 0)
+    	netif_set_link_callback(netif,config->netif_link_callback);
 #endif
     netif_set_default(netif);
     netif_set_up(netif);
