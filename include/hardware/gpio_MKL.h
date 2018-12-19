3a4e6e08e9e3069c8ee08fec5c9ff752fa73b239
Marco Giammarini <marco.giammarini@gruppofilippetti.it>
2018-12-19 09:36:49 +0100

    Added Gpio pins for MKL25 (NXP MKL)

--- include/hardware/gpio_MKL.h
+++ include/hardware/gpio_MKL.h
@@ -42,7 +42,25 @@ extern "C" {
 
 #if defined(LIBOHIBOARD_MKL)
 
-typedef enum
+/**
+ * @addtogroup LIBOHIBOARD_Driver
+ * @{
+ */
+
+/**
+ * @addtogroup GPIO
+ * @{
+ */
+
+/**
+ * @defgroup GPIO_Hardware Gpio specific hardware types
+ * @{
+ */
+
+/**
+ * List of possible GPIO pins.
+ */
+typedef enum _Gpio_Pins
 {
     GPIO_PINS_NONE = 0,
 
@@ -174,7 +192,135 @@ typedef enum
     GPIO_PINS_PTE31,
 #endif
 
-// ENDIF_ LIBOHIBOARD_MKL15
+// ENDIF LIBOHIBOARD_MKL15
+#elif defined (LIBOHIBOARD_MKL25)
+
+    GPIO_PINS_PTA0,
+    GPIO_PINS_PTA1,
+    GPIO_PINS_PTA2,
+    GPIO_PINS_PTA3,
+    GPIO_PINS_PTA4,
+#if defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTA5,
+    GPIO_PINS_PTA12,
+    GPIO_PINS_PTA13,
+#endif
+#if defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTA14,
+    GPIO_PINS_PTA15,
+    GPIO_PINS_PTA16,
+    GPIO_PINS_PTA17,
+#endif
+    GPIO_PINS_PTA18,
+    GPIO_PINS_PTA19,
+    GPIO_PINS_PTA20,
+
+    GPIO_PINS_PTB0,
+    GPIO_PINS_PTB1,
+#if defined (LIBOHIBOARD_MKL25ZxFT) || \
+    defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTB2,
+    GPIO_PINS_PTB3,
+#endif
+#if defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTB8,
+    GPIO_PINS_PTB9,
+    GPIO_PINS_PTB10,
+    GPIO_PINS_PTB11,
+#endif
+#if defined (LIBOHIBOARD_MKL25ZxFT) || \
+    defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTB16,
+    GPIO_PINS_PTB17,
+#endif
+#if defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTB18,
+    GPIO_PINS_PTB19,
+#endif
+
+#if defined (LIBOHIBOARD_MKL25ZxFT) || \
+    defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTC0,
+#endif
+    GPIO_PINS_PTC1,
+    GPIO_PINS_PTC2,
+    GPIO_PINS_PTC3,
+    GPIO_PINS_PTC4,
+    GPIO_PINS_PTC5,
+    GPIO_PINS_PTC6,
+    GPIO_PINS_PTC7,
+#if defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTC8,
+    GPIO_PINS_PTC9,
+    GPIO_PINS_PTC10,
+    GPIO_PINS_PTC11,
+#endif
+#if defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTC12,
+    GPIO_PINS_PTC13,
+    GPIO_PINS_PTC16,
+    GPIO_PINS_PTC17,
+#endif
+
+#if defined (LIBOHIBOARD_MKL25ZxFT) || \
+    defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTD0,
+    GPIO_PINS_PTD1,
+    GPIO_PINS_PTD2,
+    GPIO_PINS_PTD3,
+#endif
+    GPIO_PINS_PTD4,
+    GPIO_PINS_PTD5,
+    GPIO_PINS_PTD6,
+    GPIO_PINS_PTD7,
+
+#if defined (LIBOHIBOARD_MKL25ZxFM) || \
+    defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTE0,
+#endif
+#if defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTE1,
+#endif
+#if defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTE2,
+    GPIO_PINS_PTE3,
+    GPIO_PINS_PTE4,
+    GPIO_PINS_PTE5,
+#endif
+#if defined (LIBOHIBOARD_MKL25ZxFT) || \
+    defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTE20,
+    GPIO_PINS_PTE21,
+#endif
+#if defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTE22,
+    GPIO_PINS_PTE23,
+#endif
+#if defined (LIBOHIBOARD_MKL25ZxFT) || \
+    defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTE24,
+    GPIO_PINS_PTE25,
+    GPIO_PINS_PTE29,
+#endif
+    GPIO_PINS_PTE30,
+#if defined (LIBOHIBOARD_MKL25ZxLH) || \
+    defined (LIBOHIBOARD_MKL25ZxLK)
+    GPIO_PINS_PTE31,
+#endif
+
+// ENDIF LIBOHIBOARD_MKL15
 #endif
 
 } Gpio_Pins;
@@ -182,7 +328,7 @@ typedef enum
 /**
  * List of possible GPIO ports.
  */
-typedef enum
+typedef enum _Gpio_Ports
 {
     GPIO_PORTS_A,
     GPIO_PORTS_B,
@@ -191,6 +337,18 @@ typedef enum
     GPIO_PORTS_E,
 
 } Gpio_Ports;
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
 
 #endif // LIBOHIBOARD_MKL
 
